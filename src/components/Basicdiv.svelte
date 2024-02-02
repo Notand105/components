1c6874c19d4e5e08ed3d640905b53ac4b56feb16
Notand105 <urzua.fernando.f@gmail.com>
2024-02-02 11:06:36 -0300

    first commit

---
+++ src/components/Basicdiv.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {primary} from '../utils/colorstore'
+    import {accent} from '../utils/colorstore'
+</script>
+
+<div style="--primary-color: {$primary}; --accent-color:{$accent} ">
+    <div class="show" >
+        Patos
+    </div>
+</div>
+
+<style>
+    div{
+        color:white;
+        padding: 1rem;
+    }
+    .show{
+        background-color: var(--primary-color);
+        transition: .3s ease;
+    }
+    .show:hover{
+        background-color: var(--accent-color);
+        transition: .3s ease;
+        cursor: pointer;
+    }
+    
+</style>
+    
---
+++ src/components/ColorPicker.svelte
@@ -0,0 +1,27 @@
+<script>
+    import {primary, accent} from '../utils/colorstore'
+</script>
+
+<div class="cpicker">
+    <div class="col">
+        <h2>Primary Color : </h2>
+        <input type="color" bind:value={$primary} >
+    </div>
+    <div class="col">
+        <h2>Accent Color : </h2>
+        <input type="color" bind:value={$accent}>
+    </div>
+</div>
+
+<style>
+ .col{
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+ }
+ .cpicker{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+ }
+</style>
